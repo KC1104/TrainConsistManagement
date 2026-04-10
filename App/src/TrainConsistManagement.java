5be3be0cb35fde91acdfe7e288790bc0d056fa75
KC1104 <kushagrachandra3@gmail.com>
2026-04-10 15:02:26 +0530

    Kushagra Chandra Added UC1

---
+++ App/src/TrainConsistManagement.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainConsistManagement {
+    public static void main(String[] args) {
+        System.out.println("====================================");
+        System.out.println("  ===Train Consist Management App === ");
+        System.out.println("====================================");
+
+        List<String> trainConsist=new ArrayList<>();
+
+        System.out.println("Train initialised successfully...");
+
+        System.out.println("Initial Bogie Count: "+0);
+        System.out.println("Current Train Consist: []");
+
+        System.out.println("\nSystem Ready for operations...");
+    }
+}
