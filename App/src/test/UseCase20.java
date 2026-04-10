67b08a0acc3269daf6eed53239d07375d7239f8d
KC1104 <kushagrachandra3@gmail.com>
2026-04-10 16:34:45 +0530

    Kushagra Chandra Added UC20

---
+++ App/src/test/UseCase20.java
@@ -0,0 +1,36 @@
+package test;
+
+public class UseCase20 {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("  UC20 - Exception Handling During Search ");
+        System.out.println("====================================");
+
+        String[] bogieIds = {};
+
+        String searchId = "BG101";
+
+        if (bogieIds.length == 0) {
+            throw new IllegalStateException("No bogies available in train. Cannot perform search.");
+        }
+
+        boolean found = false;
+
+        for (String id : bogieIds) {
+            if (id.equals(searchId)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Bogie " + searchId + " found.");
+        } else {
+            System.out.println("Bogie " + searchId + " not found.");
+        }
+
+        System.out.println("\nUC20 execution completed...");
+    }
+}
